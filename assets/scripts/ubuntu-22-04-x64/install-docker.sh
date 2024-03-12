c27ec3067adc6f0e83c5fbb7aa134912c32233d9
Yihang Wang <wangyihanger@gmail.com>
2024-03-12 18:00:40 +0800

    refactor: add add-swap.sh

--- assets/scripts/ubuntu-22-04-x64/install-docker.sh
+++ assets/scripts/ubuntu-22-04-x64/install-docker.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Add swap space if not already added
-SWAP_EXIST=$(sudo swapon --show)
-SWAP_SIZE=2G
-SWAP_FILE=/swapfile
-if [ -z "$SWAP_EXIST" ]; then
-    sudo fallocate -l $SWAP_SIZE $SWAP_FILE || sudo dd if=/dev/zero of=$SWAP_FILE bs=1024 count=$(echo $SWAP_SIZE | grep -o -E '[0-9]+')k
-    sudo chmod 600 $SWAP_FILE
-    sudo mkswap $SWAP_FILE
-    sudo swapon $SWAP_FILE
-fi
-sudo swapon --show
-
 # Check if docker is not installed
 if ! [ -x "$(command -v docker)" ]; then
     sudo apt update
