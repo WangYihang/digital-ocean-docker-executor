6479c84030b4e325927d9805ce976fc93b80e8e4
Yihang Wang <wangyihanger@gmail.com>
2024-03-13 00:43:14 +0800

    feat: add docker example task

---
+++ pkg/model/task/docker-task/task.go
@@ -0,0 +1,25 @@
+package docker_task
+
+import (
+	"fmt"
+
+	"github.com/WangYihang/digital-ocean-docker-executor/pkg/model/task"
+)
+
+func Generate(label string) chan *task.DockerTask {
+	out := make(chan *task.DockerTask)
+	go func() {
+		defer close(out)
+		numShards := 3
+		for shard := 0; shard < numShards; shard++ {
+			out <- task.NewDockerTask().
+				WithDockerImage("centos:latest").
+				WithArguments("ping", "-c", "60", "8.8.8.8").
+				WithDetach().WithInteractive().WithTty().
+				WithLabel("dode.task", label).
+				WithLabel("dode.shard", fmt.Sprintf("%d", shard)).
+				WithLabel("dode.num-shard", fmt.Sprintf("%d", numShards))
+		}
+	}()
+	return out
+}
