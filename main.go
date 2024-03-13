a29a843409b6e03595a6677a2f47aea1756daffd
Yihang Wang <wangyihanger@gmail.com>
2024-03-13 19:32:22 +0800

    feat: increase concurrency

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/WangYihang/digital-ocean-docker-executor/pkg/config"
-	"github.com/WangYihang/digital-ocean-docker-executor/pkg/model/provider/digitalocean"
 	"github.com/WangYihang/digital-ocean-docker-executor/pkg/model/scheduler"
 	brute_dns_resolve_task "github.com/WangYihang/digital-ocean-docker-executor/pkg/model/task/brute-dns-resolve"
 	"github.com/charmbracelet/log"
@@ -10,8 +9,7 @@ import (
 
 func main() {
 	log.SetLevel(log.DebugLevel)
-	p := digitalocean.NewProvider()
-	s := scheduler.New().WithProvider(p)
+	s := scheduler.New().WithMaxConcurrency(4)
 	for t := range brute_dns_resolve_task.Generate(config.Cfg.Task.Label) {
 		s.SubmitDockerTask(t)
 	}
